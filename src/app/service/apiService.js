c80b5ddbb26ccaa60790a9f4605ee5dafc255dcf
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-11-23 12:34:55 -0300

    feat/host by env

--- src/app/service/apiService.js
+++ src/app/service/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: 'https://gerenciador-projeto.onrender.com',
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
